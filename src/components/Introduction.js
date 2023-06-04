03a7850bfdf000461dd3e27c511f8c715533306e
Sandy Lenormand <sandy.lenormand02@gmail.com>
2023-06-04 14:58:27 +0200

    Deployment test

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,7 @@ export default function Introduction() {
             <p>Bonjour ! <br/>
             Je suis Sandy Lenormand,<br/>
             développeuse web junior<br/>
-            et ancienne libraire !</p>
+            et ancienne libraire.</p>
             <img src={Picture} alt="portrait de Sandy Lenormand"/>
             <h2>À propos de moi</h2>
             <p>Après plusieurs années en tant que libraire, j'ai décidé de me former au métier de développeuse web, chez OpenClassrooms.</p>
